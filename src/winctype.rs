fb2fd656d6922ef7e1685a88802f12a2574832ca
Romain Labolle <romain.labolle@universite-lyon.fr>
2023-08-21 12:35:00 +0200

    Fix winctype representation

--- src/winctype.rs
+++ src/winctype.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case, non_camel_case_types, clippy::upper_case_acronyms)]
 use std::slice;
 
+#[repr(C)]
 pub struct UNICODE_STRING {
     pub Length: u16,
     pub MaximumLength: u16,
